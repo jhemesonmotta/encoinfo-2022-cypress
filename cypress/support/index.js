f173933dbf83c54627e655d46a3c43a7d2c551d5
tp.jhemeson.mota@totvspartners.com.br <tp.jhemeson.mota@totvspartners.com.br>
2022-11-22 19:41:18 -0300

    first commit

---
+++ cypress/support/index.js
@@ -0,0 +1,16 @@
+//************************************************ ***********
+// Este exemplo support/index.js é processado e
+// carregado automaticamente antes de seus arquivos de teste.
+//
+// Este é um ótimo lugar para colocar a configuração global e
+// comportamento que modifica o Cypress.
+//
+// Você pode ler mais aqui:
+// https://on.cypress.io/configuration
+//************************************************ ***********
+
+// Importe usando a sintaxe ES2015:
+import './commands'
+
+// Alternativamente, você pode usar a sintaxe CommonJS:
+// require('./commands')
